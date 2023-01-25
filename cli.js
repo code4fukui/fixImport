421e6505047d233dc189f92bfb2762a4b1cdf59d
Taisuke Fukuno <fukuno@jig.jp>
2023-01-26 07:25:42 +0900

    add cli

--- cli.js
+++ cli.js
@@ -1,5 +1,23 @@
 import { fixImport } from "./fixImport.js";
+import { dir2array } from "https://js.sabae.cc/dir2array.js";
 
-const src = await Deno.readTextFile(Deno.args[0]);
-const dst = fixImport(src);
-console.log(dst);
+const fix = async (fn) => {
+  const src = await Deno.readTextFile(fn);
+  const dst = fixImport(src);
+  await Deno.writeTextFile(fn, dst);  
+};
+
+if (Deno.args.length == 0) {
+  if (!confirm("fix all js files?")) {
+    Deno.exit(1);
+  }
+  const fns = await dir2array("./");
+  for (const fn of fns) {
+    if (!fn.endsWith(".js")) {
+      continue;
+    }
+    await fix(fn);
+  }
+} else {
+  await fix(Deno.args[0]);
+}
